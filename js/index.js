8ea0364178fb54a31d88d0d0ec53f4cdc5e07e82
unknown <atm1996.ast@gmail.com>
2020-08-22 01:06:37 -0500

    v2

--- js/index.js
+++ js/index.js
@@ -6,3 +6,28 @@ function myFunction() {
       x.className = "topnav";
     }
 }
+
+function Enviar(){
+  var correo = document.getElementById("correo").value;
+  var mensaje = document.getElementById("mensaje").value;
+  var nombre = document.getElementById("nombre").value;
+
+  if(correo!="" && mensaje!="" && nombre!="" ){
+      Email.send({
+          Host : "smtp.gmail.com",
+          secureToken:'2fb3e88b-a61b-442b-bab2-f187d59035fe',
+          Username : "atencionintegralpiixan@gmail.com",
+          Password : "jesusmisalvador2020",
+          To:"atencionintegralpiixan@gmail.com",
+          From: correo,
+          Subject:"Persona interesada,con el nombre de:" +nombre,
+          Body: mensaje,
+  
+      }).then(
+          message => alert(message)
+          // location.href="index.html"
+      );
+  }else{
+      alert("Faltan campos por llenar");
+  }
+}
